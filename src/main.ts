1fb67c8db2991247e2e1cf6e34a04fb664dc3ad0
TheLuckyman30 <bnauta0@gmail.com>
2025-09-25 21:28:04 -0400

    Modified main so that it can take in user input

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,48 @@ import { blindTreeSearch } from './utils/blind-search';
 import { dataParser } from './utils/data-parser';
 import { heuristicGraphSearch } from './utils/heurisitc-graph-search';
 import { heuristicTreeSearch } from './utils/heuristic-tree-search';
+import * as readline from 'readline/promises';
 
-console.log('\nNow performing a blind tree search\n');
+async function main(): Promise<void> {
+  const question =
+    'Select what search algorithm to use:\n' +
+    '1. Blind Search\n' +
+    '2. Heuristic Tree Search\n' +
+    '3. Heuristic Graph Search\n' +
+    '4.Exit\n';
 
-for (const importedYard of yards) {
-  const [yard, initState, goalState] = dataParser(importedYard);
-  console.log(blindTreeSearch(yard, initState, goalState));
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  let option: string = '';
+  while (option !== '4') {
+    option = await rl.question(question);
+    if (option === '1') {
+      console.log('\nPerforming a blind tree search\n');
+
+      for (const importedYard of yards) {
+        const [yard, initState, goalState] = dataParser(importedYard);
+        console.log(blindTreeSearch(yard, initState, goalState));
+      }
+    } else if (option === '2') {
+      console.log('\nPerforming a heuristic tree search\n');
+
+      for (const importedYard of yards) {
+        const [yard, initState, goalState] = dataParser(importedYard);
+        console.log(heuristicTreeSearch(yard, initState, goalState));
+      }
+    } else if (option === '3') {
+      console.log('\nNow performing a heuristic graph search\n');
+
+      for (const importedYard of yards) {
+        const [yard, initState, goalState] = dataParser(importedYard);
+        console.log(heuristicGraphSearch(yard, initState, goalState));
+      }
+    }
+  }
+  rl.close();
 }
 
-console.log('\nNow performing a heuristic tree search\n');
-
-for (const importedYard of yards) {
-  const [yard, initState, goalState] = dataParser(importedYard);
-  console.log(heuristicTreeSearch(yard, initState, goalState));
-}
-
-console.log('\nNow performing a heuristic graph search\n');
-
-for (const importedYard of yards) {
-  const [yard, initState, goalState] = dataParser(importedYard);
-  console.log(heuristicGraphSearch(yard, initState, goalState));
-}
+main();
